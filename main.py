2e3ecb3369270edaf85fd10bec617ca22797cf2b
Aaron Watkins <ac.watkins80@gmail.com>
2022-02-09 06:50:55 -0500

    remove wifi_timer as esp32 reconnects on its own move the data_check_timer to use Timer(3) instead of 2

--- main.py
+++ main.py
@@ -116,13 +116,6 @@ def get_wifi_conn_status(conn_status, bool_query_time):
         print("sorry, cant connect to wifi AP! connection --> {}".format(conn_status))
     return conn_status
 
-def update_conn_status(wifi_timer):
-    global connection
-    if not wlan.isconnected():
-        connection = get_wifi_conn_status(connect_wifi(), True)
-    else:
-        connection = set_time()
-
 def reset_rain_counter_daily(rain_timer):
     weather_obj.reset_daily_rain_count()
     rain_timer.init(period=ms_until_midnight(), mode=Timer.ONE_SHOT, callback=reset_rain_counter_daily)
@@ -198,10 +191,9 @@ def ms_until_midnight():
 connection = ""
 wifi_led_red()
 config = read_config_file(CONFIG_FILE)
-wifi_timer = Timer(0)
 weather_timer = Timer(0)
 rain_timer = Timer(2)
-data_check_timer = Timer(2)
+data_check_timer = Timer(3)
 init_wlan()
 connection = get_wifi_conn_status(connect_wifi(), True)
 
@@ -211,7 +203,6 @@ wind_speed_last_intrpt = begin_time
 gust_start_timer = begin_time
 rain_counter_pin.irq(trigger=Pin.IRQ_RISING, handler=rain_counter_isr)
 wind_speed_pin.irq(trigger=Pin.IRQ_RISING, handler=wind_speed_isr)
-wifi_timer.init(period=WIFI_CHECK_PERIOD, mode=Timer.PERIODIC, callback=update_conn_status)
 weather_timer.init(period=WEATHER_UPDATE_PERIOD, mode=Timer.PERIODIC, callback=update_weather)
 rain_timer.init(period=ms_until_midnight(), mode=Timer.ONE_SHOT, callback=reset_rain_counter_daily)
 data_check_timer.init(period=DATA_POINT_CHECK_PERIOD, mode=Timer.PERIODIC, callback=record_weather_data_points)
