d805331f87fcb32a1b4636b914f518e3ba3e2b09
Aaron Watkins <awatkins@wabtec.com>
2022-02-19 12:22:59 -0500

    change i2c pin numbers main am2320 example mpl3115a2 example

--- examples/am2320_i2c.py
+++ examples/am2320_i2c.py
@@ -1,10 +1,12 @@
 ########################
-# PI PICO
+# ESP32 c3 Mini
 ########################
 from machine import I2C, Pin
 from am2320 import AM2320
-i2c = I2C(0,scl=Pin(17),sda=Pin(16),freq=400000)
+i2c = I2C(0,scl=Pin(19),sda=Pin(18),freq=400000)
 am_2320 = AM2320(i2c)
 am_2320.measure()
 humidity = am_2320.humidity()
 temperature = am_2320.temperature()
+humidity
+temperature
--- examples/mpl3115a2_i2c.py
+++ examples/mpl3115a2_i2c.py
@@ -1,13 +1,17 @@
 ########################
-# PI PICO
+# ESP32 c3 Mini
 ########################
 from machine import I2C, Pin
 from mpl3115a2 import MPL3115A2
-i2c = I2C(0,scl=Pin(17),sda=Pin(16),freq=400000)
+i2c = I2C(0,scl=Pin(19),sda=Pin(18),freq=400000)
 mpl = MPL3115A2(i2c, mode=MPL3115A2.ALTITUDE)
 altitude = mpl.altitude()
 temperature = mpl.temperature()
+altitude
+temperature
 
 mpl = MPL3115A2(i2c, mode=MPL3115A2.PRESSURE)
 pressure = mpl.pressure()
 temperature = mpl.temperature()
+pressure
+temperature
